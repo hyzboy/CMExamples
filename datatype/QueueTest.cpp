fe289163361f4aa37f8c2eaf4310338eed1cd98c
HuYingzhuo(hugo/hyzboy) <hyzboy@gmail.com>
2023-08-10 12:02:03 +0800

    added PoolTest.cpp

--- datatype/QueueTest.cpp
+++ datatype/QueueTest.cpp
@@ -1,5 +1,6 @@
 ﻿#include<hgl/type/Queue.h>
 #include<iostream>
+#include"UserInfo.h"
 
 using namespace hgl;
 using namespace std;
@@ -61,46 +62,6 @@ void TestQueueUnordered()
         }
     }
 }
-
-struct UserInfo
-{
-    char name[32];
-    bool sex;
-    int age;
-};
-
-static UserInfo ui_array[]=
-{
-    //注： 以下人物信息由Github Copilot自动创建
-    //ps: The following list of character information is automatically created by Github Copilot
-
-    {"Adloph",true,18},
-    {"Bella",false,19},
-    {"Cindy",false,20},
-    {"David",true,21},
-    {"Elsa",false,22},
-    {"Frank",true,23},
-    {"Gina",false,24},
-    {"Helen",false,25},
-    {"Ivan",true,26},
-    {"Jack",true,27},
-    {"Kitty",false,28},
-    {"Lily",false,29},
-    {"Mike",true,30},
-    {"Nancy",false,31},
-    {"Owen",true,32},
-    {"Peter",true,33},
-    {"Queen",false,34},
-    {"Robert",true,35},
-    {"Sunny",false,36},
-    {"Tom",true,37},
-    {"Uma",false,38},
-    {"Vivian",false,39},
-    {"Wendy",false,40},
-    {"Xavier",true,41},
-    {"Yoyo",false,42},
-    {"Zack",true,43}
-};
 
 void TestQueueStruct()
 {
