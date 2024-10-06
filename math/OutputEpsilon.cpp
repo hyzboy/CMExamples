7b70926751c1e9b3e7a7b6e004b51e51810ea6ef
hyzboy <hyzboy@gmail.com>
2024-10-07 00:59:30 +0800

    update OutputEpsilon.cpp

--- math/OutputEpsilon.cpp
+++ math/OutputEpsilon.cpp
@@ -6,9 +6,13 @@ using namespace hgl;
 
 int main(int,char **)
 {
-    std::cout<<"float: "<<HGL_FLOAT_EPSILON<<std::endl;
-    
-    std::cout<<"double: "<<HGL_DOUBLE_EPSILON<<std::endl;
+    std::cout<<"float min:"<<HGL_FLOAT_MIN<<std::endl;
+    std::cout<<"float max:"<<HGL_FLOAT_MAX<<std::endl;
+    std::cout<<"float epsilon: "<<HGL_FLOAT_EPSILON<<std::endl;
+
+    std::cout<<"double min:"<<HGL_DOUBLE_MIN<<std::endl;
+    std::cout<<"double max:"<<HGL_DOUBLE_MAX<<std::endl;
+    std::cout<<"double epsilon: "<<HGL_DOUBLE_EPSILON<<std::endl;
 
     return 0;
 }
