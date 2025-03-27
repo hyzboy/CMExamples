25b5c60fe53bdba30d39494b840b5e6641535165
hyzboy <hyzboy@gmail.com>
2025-03-27 23:41:40 +0800

    filesystem::Combine测试

--- filesystem/FixFilenameTest.cpp
+++ filesystem/FixFilenameTest.cpp
@@ -5,7 +5,7 @@ using namespace std;
 using namespace hgl;
 using namespace hgl::filesystem;
 
-void main()
+int os_main(int,os_char **)
 {
     constexpr os_char incorrect_filename[]=OS_TEXT("c:/win95\\\\ms-dos");
 
@@ -13,4 +13,12 @@ void main()
 
     os_out<<OS_TEXT("origin filename: ")<<incorrect_filename<<endl;
     os_out<<OS_TEXT(" fixed filename: ")<<FixFilename(incorrect_filename).c_str()<<endl;
+
+    cout<<"Combine(1,2,3): "<<Combine({"1","2","3"}).c_str()<<endl;
+
+    os_out<<OS_TEXT("Combine(A,B,C): ")<<Combine({OS_TEXT("A"),OS_TEXT("B"),OS_TEXT("C")}).c_str()<<endl;
+
+    os_out<<OS_TEXT("Combine(a,b,c): ")<<Combine({OSString(OS_TEXT("A")),OSString(OS_TEXT("B")),OSString(OS_TEXT("C"))}).c_str()<<endl;
+
+    return 0;
 }
