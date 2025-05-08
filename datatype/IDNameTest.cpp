959eadd68d1365aa0a9ad87cafdeb86d4ffa35ec
hyzboy <hyzboy@gmail.com>
2025-05-09 01:11:09 +0800

    更新IDNameTest范例，使输出更清理好理解。

--- datatype/IDNameTest.cpp
+++ datatype/IDNameTest.cpp
@@ -5,20 +5,20 @@ int main(int,char **)
 {
     using namespace hgl;
 
-    AIDName id3("id1");
-    AIDName id4("id2");
+    AIDName id3("idA");
+    AIDName id4("idB");
 
-    AIDName id5("id1");
-    AIDName id6("id2");
+    AIDName id5("idA");
+    AIDName id6("idB");
 
-    std::cout<<id5.GetID()<<":"<<id5.GetName()<<", ClassID: "<<std::hex<<id5.GetClassID()<<std::endl;
-    std::cout<<id6.GetID()<<":"<<id6.GetName()<<", ClassID: "<<std::hex<<id6.GetClassID()<<std::endl;
+    std::cout<<"id5 id:"<<id5.GetID()<<" name:\""<<id5.GetName()<<"\", ClassHC: "<<std::hex<<id5.GetClassHC()<<std::endl;
+    std::cout<<"id6 id:"<<id6.GetID()<<" name:\""<<id6.GetName()<<"\", ClassHC: "<<std::hex<<id6.GetClassHC()<<std::endl;
 
-    std::cout<<id3.GetID()<<":"<<id3.GetName()<<", ClassID: "<<std::hex<<id3.GetClassID()<<std::endl;
-    std::cout<<id4.GetID()<<":"<<id4.GetName()<<", ClassID: "<<std::hex<<id4.GetClassID()<<std::endl;
+    std::cout<<"id3 id:"<<id3.GetID()<<" name:\""<<id3.GetName()<<"\", ClassHC: "<<std::hex<<id3.GetClassHC()<<std::endl;
+    std::cout<<"id4 id:"<<id4.GetID()<<" name:\""<<id4.GetName()<<"\", ClassHC: "<<std::hex<<id4.GetClassHC()<<std::endl;
 
     if(id3==id5)
-        std::cout<<"id3==id5"<<std::endl;
+        std::cout<<"id3==id5 is OK!"<<std::endl;
     else
-        std::cout<<"id3!=id5"<<std::endl;
+        std::cout<<"id3!=id5 is BUG!"<<std::endl;
 }
